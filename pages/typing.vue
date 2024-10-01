10fdfe82d33897a63e928fde048a8842a4f04c7a
Craillon <vistin10mi@gmail.com>
2024-10-01 17:55:40 +0200

    sound integration

--- pages/typing.vue
+++ pages/typing.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
-        <div class="h-screen w-full bg-gray-">
-            <div class="w-11/12 mx-auto h-full relative">
+        <div class="h-screen w-full bg">
+            <div class="bg-white/90 backdrop-blur-md h-full w-full absolute bg-gradient-to-t z-10 from-white"></div>
+            <div class="w-11/12 mx-auto h-full relative z-50">
                 <div class="">
                     <UDropdown  class="text-green-500 absolute z-50" :items="items" :ui="{ item: { disabled: 'cursor-text select-text' } }" :popper="{ placement: 'bottom-start' }">
                         <button type="button" class="bg-green-500 flex items-center gap-x-3 bg-gradient-to-r from-green-400 p-2 py-2.5 px-6 rounded shadow-lg mt-4 pr-6 shadow-green-500/40 text-white">
                             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M18.425 10.271C19.499 8.967 18.57 7 16.88 7H7.12c-1.69 0-2.618 1.967-1.544 3.271l4.881 5.927a2 2 0 0 0 3.088 0l4.88-5.927Z" clip-rule="evenodd"/>
                             </svg>
-                            Difficulté
+                            {{ label }}
                         </button>
+                        <!--  -->
                         <template #item="{ item }">
                             <span @click="changeLevel(item.label)" class="truncate text-slate-800 w-full h-full text-start"> {{ item.label }} </span>
                         </template>
@@ -33,21 +35,34 @@
                         </div>
                     </button>
                 </div>
-                <!--  -->
-                <div class="h-1/5 flex items- justify- w-full  border-green-50 p-4 relative">
-                    <p class="text text-slate-400 italic absolute -top-7 left-4"> <span class="text-2xl">" </span>Text à saisir</p>
-                    <p class="text-slate-500 text-xl">{{ lorem }}</p>
-                </div>
-                <!--  -->
-                <div class="h-1/5 w-11/12 mx-auto flex items-center justify-start">
-                    <div class="text-xl text-nowrap overflow-hidden relative" >
-                        <div class="ml-[37rem] text-white space-x-2 flex items-center p-8">
-                            <template v-for="(item, idx) in worldTyping.split('')" :key="idx" class="p-2">
-                                <span v-if=" idx === 0 " class="bg-green-400 p-2 h-14 px-4 shadow-lg shadow-green-400/50 rounded text-2xl font-bold duration-150">{{ item }}</span>
-                                <span v-else class="bg-slate-100 rounded h-11 px-2.5 duration-150 p-2 text-slate-800">{{ item }}</span>
-                            </template>
+                <div v-if="worldTyping" class="h-2/5">
+                    <!--  -->
+                    <div class="h-2/4 flex items-center justify-center w-full  border-green-50 p-4 relative">
+                        <p class="text text-slate-400 italic absolute -top-7 "> <span class="text-2xl">" </span>Text à saisir</p>
+                        <p class="text-slate-500 text-xl">{{ lorem }}</p>
+                    </div>
+                    <!--  -->
+                    <div class="h-2/4 w-11/12 mx-auto flex items-center justify-start">
+                        <div class="text-xl text-nowrap overflow-hidden relative" >
+                            <div class="ml-[37rem] text-white space-x-2 flex items-center p-8">
+                                <template v-for="(item, idx) in worldTyping.split('')" :key="idx" class="p-2">
+                                    <span v-if=" idx === 0 " class="bg-green-400 p-2 h-14 px-4 shadow-lg shadow-green-400/50 rounded text-2xl font-bold duration-150">{{ item }}</span>
+                                    <span v-else class="bg-slate-100 rounded h-11 px-2.5 duration-150 p-2 text-slate-800">{{ item }}</span>
+                                </template>
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div v-else class="h-2/5 bg-transparent bg-gradient-to-t from-slate-100/20 rounded-lg border-dashed">
+                    <h3 class="text-slate-600 font-semibold text-center text-3xl">Partie terminée !</h3>
+                    <div class="flex justify-center  w-full mt-10">
+                        <a href="/typing" class="rounded-full bg-green-200 p-1.5 px-6 shadow-green-100 shadow-md hover:bg-green-300 duration-300">Rejouez</a>
+                    </div>
+                </div>
+                <div class="absolute left-0 right-0 bottom-0 flex items-center justify-center">
+                    <a href="/" class=" mb-10 bg-slate-800 shadow-lg shadow-slate-500/30 text-white hover:bg-slate-700 py-2 hover:shadow-xl duration-300 rounded-full px-6">
+                        Quittez la partie
+                    </a>
                 </div>
             </div>
         </div>
@@ -59,35 +74,19 @@ import { faker } from '@faker-js/faker';
 
 const items = [
     [
-        {
-            label: 'Mot'
-        }, 
-        {
-            label: 'Mots'
-        }, 
-        {
-            label: 'Phrase'
-        },
-        {
-            label: 'Phrases'
-        },  
-        {
-            label: 'Ligne'
-        }, 
-        {
-            label: 'Paragraphe'
-        }, 
-        {
-            label: 'Paragraphes'
-        }, 
-        {
-            label: 'Text'
-        }
+        { label: 'Mot' },
+        { label: 'Mots' },
+        { label: 'Phrase' },
+        { label: 'Phrases' },
+        { label: 'Ligne' },
+        { label: 'Paragraphe' },
+        { label: 'Paragraphes' },
+        { label: 'Text' }
     ]
 ]
 
-faker.lorem.paragraph
 
+const label = ref("Difficulté")
 const lorem = ref(faker.lorem.word())
 const worldTyping = ref(lorem.value)
 const value = ref("")
@@ -101,6 +100,7 @@ const chrono = ref({
 })
 
 const changeLevel = (data) => {
+    label.value = data
     new Promise((resolve, reject) => setTimeout(resolve => {
         if (data == "Mot") {
             lorem.value = faker.lorem.word()
@@ -198,6 +198,11 @@ onMounted(() => {
 })
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.bg {
+    background: url("~/assets/images/bg-background.jpg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+}
 </style>

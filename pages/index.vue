008400e8da548cbcd379b96d7ef78926942020b2
Craillon <vistin10mi@gmail.com>
2024-10-02 13:24:53 +0200

    ui retouche et organisation code

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,18 @@
 <template>
     <div>
         <div class="h-screen w-full relative">
-            <!-- <a href="https://github.com/Craillon/ascii-code" class="absolute top-2 right-2 text-xs z-50 py-1 rounded-full mr-10 mt-3 text-white pr-4 pl-2 shadow-md flex items-center gap-x-2 bg-slate-800">
-                <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                    <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"/>
-                </svg>
-                Github
-            </a> -->
-            <!-- <a href="https://github.com/Craillon/ascii-code" class="absolute right-3 top-3 z-50">Moi aussi je suis un lien</a> -->
             <div class="bg-white/80 backdrop-blur-md h-full w-full absolute bg-gradient-to-t z-30 from-white"></div>
             <div class="w-11/12 mx-auto h-full relative z-40">
                 <div class="h-4/5 w-full flex items-center justify-center">
                     <div class="w-full text-center space-y-14">
                         <h4 class="text-2xl font-light text-green-700">Keyboard <span class="font-semibold text-slate-500">Game</span></h4>
                         <img src="~/assets/images/1_BbDBPnpY_yt1q7CXtsYEJg.jpg" alt="" class="w-4/12 mx-auto mt-5">
+                        
                     </div>
                 </div>
                 <div class="absolute left-0 right-0 bottom-10 flex items-center justify-center">
                     <NuxtLink to="/typing" class=" mb-10 bg-green-400 shadow-lg shadow-green-500/40 text-white hover:bg-green-400 bg-gradient-to-r hover:from-green-500 py-2 hover:shadow-xl hover:duration-700 rounded-full px-6">
-                        Calculez votre temps de saisie !
+                        Evaluez votre temps de saisie !
                     </NuxtLink>
                 </div>
             </div>
--- pages/typing.vue
+++ pages/typing.vue
@@ -1,12 +1,63 @@
 <template>
     <div>
         <div class="h-screen w-full bg">
-            <div class="bg-white/90 backdrop-blur-md h-full w-full absolute bg-gradient-to-t z-10 from-white"></div>
-            <div class="w-11/12 mx-auto h-full relative z-50">
-                <div class="">
-                    
+            <div class="bg-white/95 backdrop-blur-md h-full w-full absolute bg-gradient-to-t z-10 from-white"></div>
+            <div class="w-11/12 mx-auto h-full relative z-40">
+                <!--  -->
+                <div v-if="worldTyping" class="absolute top-3 z-40 w-8/12 mx-auto grid grid-cols-4 gap-x-4 left-0 right-0">
+                    <div class="col-span-1 bg-white border-slate-300 p-2 rounded flex items-center gap-7">
+                        <div class="text-slate-500 p-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                            </svg>
+                        </div>
+                        <div class="">
+                            <h5 class="text-2xl">
+                                <p>{{ lorem.length }}</p>
+                                <p class="text-xs font-normal">{{ lorem.length > 1 ? 'caractères' : 'caractère' }}</p>
+                            </h5>
+                        </div>
+                    </div>
+                    <!--  -->
+                    <div class="col-span-1 bg-white border-slate-300 p-2 rounded flex items-center gap-7">
+                        <div class="text-slate-500 rounded p-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+                            </svg>
+                        </div>
+                        <div class="">
+                            <h5 class="text-2xl">
+                                <p>{{ lorem.split(' ').length }}</p>
+                                <p class="text-xs font-normal">{{ lorem.split(' ').length > 1 ? 'mots' : 'mot'}}</p>
+                            </h5>
+                        </div>
+                    </div>
+                    <!--  -->
+                    <div class="col-span-2 relative bg-white border-slate-300 p-2 px-4 rounded flex items-center justify-center gap-7">
+                        <button type="button" @click="manupChrono()" class="absolute left-6 top-2.5 gap-x-4 flex items-center rounded bg-white text-white">
+                            <div class="bg-white rounded-full text-green-500 h-10 w-10 flex items-center justify-center  shadow-slate-300/25 duration-200">
+                                <svg v-if="!startTime" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
+                                    <path fill-rule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clip-rule="evenodd" />
+                                </svg>
+                                <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+                                </svg>
+                            </div>
+                        </button>
+                        <div class="flex items-baseline gap-4 relative justify-center text-slate-600">
+                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                            </svg>
+                            <p class="w-11"><span class="text-xl font-medium">{{ chrono.M }}&nbsp;</span>min</p>
+                            <p class="w-11"><span class="text-xl font-medium">{{ chrono.S }}&nbsp;</span>sec&nbsp;</p>
+                            <p class="w-10 text-center text-xl">{{ chrono.T !== 0 ? chrono.T : '00' }}</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- LEVEL MODAL -->
+                <div v-if="worldTyping" class="">
                     <UDropdown  class="text-green-500 absolute z-50" :items="items" :ui="{ item: { disabled: 'cursor-text select-text' } }" :popper="{ placement: 'bottom-start' }">
-                        <button :class="[label == '' ? 'w-60' : '']" type="button" class="bg-green-500 w-60 h-11 flex items-center gap-x-3 bg-gradient-to-r from-green-400 p-2 py-2.5 px-6 rounded shadow-lg mt-4 pr-6 shadow-green-500/40 text-white">
+                        <button :class="[label == '' ? 'w-' : '']" type="button" class="bg-green-500 w-48 h-11 flex items-center gap-x-3 bg-gradient-to-r from-green-400 p-2 py-2.5 px-6 rounded shadow-lg mt-4 pr-6 shadow-green-500/40 text-white">
                             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M18.425 10.271C19.499 8.967 18.57 7 16.88 7H7.12c-1.69 0-2.618 1.967-1.544 3.271l4.881 5.927a2 2 0 0 0 3.088 0l4.88-5.927Z" clip-rule="evenodd"/>
                             </svg>
@@ -14,36 +65,18 @@
                         </button>
                         <!--  -->
                         <template #item="{ item }">
-                            <a href="/typing" @click="changeLevel(item.label)" class="truncate text-slate-800 w-full h-full text-start"> {{ item.label }} </a>
+                            <a :href="`/typing#${item.label}`" @click="changeLevel(item.label)" class="truncate text-slate-800 w-full h-full text-start"> {{ item.label }} </a>
                         </template>
                     </UDropdown>
                 </div>
-                <!--  -->
-                <div class="h-2/5 w-full bg-slate-5 flex items-center justify-center relative">
-                    <button type="button" @click="manupChrono()" class="absolute right-2 top-4 gap-x-4 flex items-center bg-green-500 bg-gradient-to-r from-green-400 p-2 rounded shadow-lg pr-6 shadow-green-500/40 text-white">
-                        <div class="bg-white rounded-full text-green-500 h-8 w-8 flex items-center justify-center hover:shadow-lg duration-200">
-                            <svg v-if="!startTime" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
-                                <path fill-rule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clip-rule="evenodd" />
-                            </svg>
-                            <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
-                            </svg>
-                        </div>
-                        <div class="flex items-baseline gap-4 relative">
-                            <p class="w-11"><span class="text-xl font-medium">{{ chrono.M }}&nbsp;</span>min</p>
-                            <p class="w-11"><span class="text-xl font-medium">{{ chrono.S }}&nbsp;</span>sec&nbsp;</p>
-                            <p class="w-5 text-xl">{{ chrono.T !== 0 ? chrono.T : '00' }}</p>
-                        </div>
-                    </button>
-                </div>
-                <div v-if="worldTyping" class="h-2/5">
+                <!-- TEXT TYPING -->
+                <div v-if="worldTyping" class="h-4/5 block space-y-3">
                     <!--  -->
-                    <div class="h-2/4 flex items-center justify-center w-full  border-green-50 p-4 relative">
-                        <p class="text text-slate-400 italic absolute -top-7 "> <span class="text-2xl">" </span>Text à saisir</p>
-                        <p class="text-slate-500 text-xl">{{ lorem }}</p>
+                    <div class="h-4/5 flex items-end justify-center w-full  border-green-50 p-4 relative">
+                        <p class="text-slate-500 text-xl bg-slate-50 p-1">{{ lorem }}</p>
                     </div>
                     <!--  -->
-                    <div class="h-2/4 w-11/12 mx-auto flex items-center justify-start">
+                    <div class="h-1/5 w-11/12 mx-auto flex items-center justify-start">
                         <div class="text-xl text-nowrap overflow-hidden relative" >
                             <div class="ml-[37rem] text-white space-x-2 flex items-center p-8">
                                 <template v-for="(item, idx) in worldTyping.split('')" :key="idx" class="p-2">
@@ -54,13 +87,25 @@
                         </div>
                     </div>
                 </div>
-                <div v-else class="h-2/5 bg-transparent bg-gradient-to-t from-slate-100/20 rounded-lg border-dashed">
-                    <h3 class="text-slate-600 font-semibold text-center text-3xl">Partie terminée !</h3>
-                    <div class="flex justify-center  w-full mt-10">
-                        <a href="/typing" class="text-slate-800 rounded-full bg-green-200 p-1.5 px-6 shadow-green-100 shadow-md hover:bg-green-300 duration-300">Rejouez</a>
+                <!-- WIN POPUP -->
+                <div v-else class="h-full re absolute flex items-center justify-center left-0 right-0 w-full z-50 bg-gradient-to-t my-auto from-slate-100/20">
+                    <div class="h-4/6 relative w-7/12 ">
+                        <img src="~/assets/images/Bingo-pana.png" alt="success image" class="w-60 mx-auto bg-slate-50/50 rounded">
+                        <h3 class="text-slate-600 font-semibold text-center text-3xl">Partie terminée !</h3>
+                        <div class="">
+                            <p class="text-xs mt-6 text-center">
+                                Vous avez terminé la partie de <span class="font-bold">{{ lorem.split(' ').length > 1 ? lorem.split(' ').length+' mots' : lorem.split(' ').length+' mot' }} </span>
+                                contant <span class="font-bold">{{ lorem.length > 1 ? lorem.length+' caractères' : lorem.length+' caractère' }}</span> <br>votre score est de
+                                <span class="font-bold">{{ `${chrono.H} heure ${chrono.M} min ${chrono.S} sec ${chrono.T} tier` }}</span>
+                            </p>
+                        </div>
+                        <div class="flex justify-center w-full mt-10">
+                            <a href="/typing" class="text-slate-800 rounded-full bg-green-200 p-1.5 px-6 shadow-green-100 shadow-md hover:bg-green-300 duration-300">Rejouez</a>
+                        </div>
                     </div>
                 </div>
-                <div class="absolute left-0 right-0 bottom-0 flex items-center justify-center">
+                <!-- EXIT BTN -->
+                <div class="absolute z-50 left-0 right-0 bottom-0 flex items-center justify-center">
                     <a href="/" class=" mb-10 bg-slate-800 shadow-lg shadow-slate-500/30 text-white hover:bg-slate-700 py-2 hover:shadow-xl duration-300 rounded-full px-6">
                         Quittez la partie
                     </a>
@@ -88,9 +133,9 @@ const items = [
 
 const level = ref("")
 
-const label = ref("")
-const lorem = ref(faker.lorem.word())
-const worldTyping = ref(lorem.value)
+const label = ref("..................")
+const lorem = ref("")
+const worldTyping = ref(".")
 const value = ref("")
 const index = ref(0);
 const startTime = ref(false)
@@ -101,78 +146,57 @@ const chrono = ref({
     T: 0,
 })
 
+const idInterval = ref()
+
 const changeLevel = (data) => {
     label.value = data
-    // new Promise((resolve, reject) => setTimeout(resolve => {
-        let fker = ""
-        if (data == "Mot") {
-            fker = faker.lorem.word()
-            lorem.value = fker
-            worldTyping.value = lorem.value
-        }
-        if (data == "Mots") {
-            const fker = faker.lorem.words()
-            lorem.value = fker
-            worldTyping.value = lorem.value
-            
-        }
-        if (data == "Phrase") {
-            const fker = faker.lorem.sentence()
-            lorem.value = fker
-            worldTyping.value = lorem.value
-            
-        }
-        if (data == "Phrases") {
-            const fker = faker.lorem.sentences()
-            lorem.value = fker
-            worldTyping.value = lorem.value
-            
-        }
-        if (data == "Ligne") {
-            const fker = faker.lorem.line()
-            lorem.value = fker
-            worldTyping.value = lorem.value
-            
-        }
-        if (data == "Lignes") {
-            const fker = faker.lorem.lines()
-            lorem.value = fker
-            worldTyping.value = lorem.value
-            
-        }
-        if (data == "Paragraphe") {
-            const fker = faker.lorem.paragraph()
-            lorem.value = fker
-            worldTyping.value = lorem.value
-            
-        }
-        if (data == "Paragraphes") {
-            const fker = faker.lorem.paragraphs()
-            lorem.value = fker
-            worldTyping.value = lorem.value
-            
-        }
-        if (data == "Text") {
-            const fker = faker.lorem.text()
-            lorem.value = fker
-            worldTyping.value = lorem.value
-            
-        }
-        localStorage.setItem("level", data)
+    if (data == "Mot") {
+        lorem.value = faker.lorem.word()
+        worldTyping.value = lorem.value
+    }
+    if (data == "Mots") {
+        lorem.value = faker.lorem.words()
+        worldTyping.value = lorem.value
+    }
+    if (data == "Phrase") {
+        lorem.value = faker.lorem.sentence()
+        worldTyping.value = lorem.value
+    }
+    if (data == "Phrases") {
+        lorem.value = faker.lorem.sentences()
+        worldTyping.value = lorem.value
+    }
+    if (data == "Ligne") {
+        lorem.value = faker.lorem.line()
+        worldTyping.value = lorem.value
+    }
+    if (data == "Lignes") {
+        lorem.value = faker.lorem.lines()
+        worldTyping.value = lorem.value
+    }
+    if (data == "Paragraphe") {
+        lorem.value = faker.lorem.paragraph()
+        worldTyping.value = lorem.value
+    }
+    if (data == "Paragraphes") {
+        lorem.value = faker.lorem.paragraphs()
+        worldTyping.value = lorem.value
+    }
+    if (data == "Text") {
+        lorem.value = faker.lorem.text()
+        worldTyping.value = lorem.value
+    }
+    localStorage.setItem("level", data)
+    clearInterva(idInterval.value)
+    resetChrono()
         
-    // }, 100))
 }
 
 const manupChrono = () => {
     startTime.value = !startTime.value
 }
 
-onMounted(() => {
-    level.value = !!localStorage.getItem("level") ? localStorage.getItem("level") : "Mot"
-    changeLevel(level.value)
-    // lorem = ref(faker.lorem.word())
-    worldTyping.value = lorem.value
-
+const windowEvent = () => {
     window.addEventListener('keydown', ev => {
         startTime.value = true
         if (lorem.value.split('').at(index.value).charCodeAt(0) == ev.key.charCodeAt(0) || (lorem.value.split('').at(index.value -1) === "." && ev.key === " ") ) {
@@ -180,32 +204,63 @@ onMounted(() => {
             worldTyping.value = worldTyping.value.split('').slice(1).join('')
             index.value++
         }
+        if (worldTyping.value.length === 0) {
+            // Code pour bloquer la modal de message success
+        }
     })
-    const id = setInterval(timerChrono, 1000/60);
-    function timerChrono () {
-        if (worldTyping.value.length === 0 ) {
-            startTime.value = false
-            clearInterval(id);
-        } else {
-            if (startTime.value == true) {
-                chrono.value.T++ 
-                if (chrono.value.T === 60) {
-                    chrono.value.S++
-                    if (chrono.value.S == 60) {
-                        chrono.value.M++
-                        chrono.value.S = 0
-                        if (chrono.value.M == 60) {
-                            chrono.value.H++
-                            chrono.value.M = 0
-                        }
+}
+
+const timerChrono = () => {
+    if (worldTyping.value.length === 0 ) {
+        clearInterva(idInterval.value);
+    } else {
+        if (startTime.value == true) {
+            chrono.value.T++ 
+            if (chrono.value.T === 60) {
+                chrono.value.S++
+                if (chrono.value.S == 60) {
+                    chrono.value.M++
+                    chrono.value.S = 0
+                    if (chrono.value.M == 60) {
+                        chrono.value.H++
+                        chrono.value.M = 0
                     }
-                    chrono.value.T = 0
                 }
+                chrono.value.T = 0
             }
         }
     }
+}
 
+const intervalChrono = () => setInterval(timerChrono, 1000/60);
+
+const clearInterva = (id) => {
+    startTime.value = false
+    clearInterval(id)
+}
+
+const resetChrono = () => {
+    chrono.value.H = 0;
+    chrono.value.M = 0;
+    chrono.value.S = 0;
+    chrono.value.T = 0
+}
+
+onBeforeRouteUpdate( (from, to) => {
+    index.value = 0
+    resetChrono()
+    windowEvent()
+    idInterval.value = intervalChrono()
 })
+
+onBeforeMount(() => {
+    level.value = !!localStorage.getItem("level") ? localStorage.getItem("level") : "Mot"
+    changeLevel(level.value)
+    worldTyping.value = lorem.value
+    windowEvent()
+    idInterval.value = intervalChrono()
+})
+
 </script>
 
 <style scoped>
